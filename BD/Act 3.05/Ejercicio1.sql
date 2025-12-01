489bcf82a5194e79714fbc425d05d497c70ad874
Schnozx <javierja29402@gmail.com>
2025-12-01 17:26:26 +0100

    update db 3.05

--- BD/Act 3.05/Ejercicio1.sql
+++ BD/Act 3.05/Ejercicio1.sql
@@ -7,7 +7,7 @@ CREATE TABLE PUB(
     Licencia_Fiscal VARCHAR (20) UNIQUE NOT NULL,
     Domicilio VARCHAR (100),
     Fecha_Apertura DATE NOT NULL,
-    Horario VARCHAR (50) CHECK (Horario IN ('HOR1', 'HOR2', 'HOR3')) NOT NULL,,
+    Horario VARCHAR (50) ENUM ('HOR1', 'HOR2', 'HOR3')) NOT NULL,,
     Cod_Localidad INT NOT NULL
 )
 
@@ -41,7 +41,7 @@ CREATE TABLE LOCALIDAD (
 CREATE PUB_EMPLEADO (
     Cod_Pub VARCHAR (40) NOT NULL,
     Dni_Empleado VARCHAR (10) NOT NULL,
-    Funcion VARCHAR (40) CHECK (Funcion IN ('CAMARERO', 'SEGURIDAD', 'LIMPIEZA')) NOT NULL,
+    Funcion VARCHAR (40) ENUM ('CAMARERO', 'SEGURIDAD', 'LIMPIEZA')) NOT NULL,
     PRIMARY KEY (Cod_Pub, Dni_Empleaedo, Funcion),
 )
 
