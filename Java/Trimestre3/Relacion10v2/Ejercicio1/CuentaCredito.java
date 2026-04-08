78199412f961235cc97fdd3130f06fc7cebd3980
Schnoxz <javierja29402@gmail.com>
2026-04-08 03:34:33 +0200

    PSOE Update

--- Java/Trimestre3/Relacion10v2/Ejercicio1/CuentaCredito.java
+++ Java/Trimestre3/Relacion10v2/Ejercicio1/CuentaCredito.java
@@ -1,57 +1,61 @@
 package Relacion10v2.Ejercicio1;
 
 public class CuentaCredito extends Cuenta {
-	private double credito;
 
-	// Clase constructor Cuenta Credito
-	public CuentaCredito(String titular, double saldo, double credito) {
-		super(titular, saldo);
-		setCredito(credito); // Llamo al setter para cumplir con la validación
-	}
+    private double credito;
 
-	// Sobrecarga para cumplir con la condicion de un constructor si no se indica parametro de saldo o credito
-	// Se cree con saldo inicializado en 0 y credito 100
-	public CuentaCredito(String titular) {
-		super(titular, 0);
-		this.credito = 100;
-	}
+    // Clase constructor Cuenta Credito
+    public CuentaCredito(String titular, double saldo, double credito) {
+        super(titular, saldo);
+        setCredito(credito); // Llamo al setter para cumplir con la validación
+    }
 
-	// Getter
-	public double getCredito() { return credito; }
+    // Sobrecarga para cumplir con la condicion de un constructor si no se indica parametro de saldo o credito
+    // Se cree con saldo inicializado en 0 y credito 100
+    public CuentaCredito(String titular) {
+        super(titular, 0);
+        this.credito = 100;
+    }
 
-	// Setter
-	public void setCredito (double credito) {
-		// Validación que no supere 300€
-		if (credito > 300) {
-			System.out.println("El crédito no debe superar los 300€");
-			return; // Se devuelve para que vuelva a introducir el valor, tras imprimir el mensaje de error
-		}
-		// Validación para que el saldo no sea inválido
-		if (this.saldo < -credito) { // Pongo el credito en negativo porque nos dice que podemos tener numeros rojos igual a la cantidad de crédito pero este mismo no puede ser mayor al saldo actual
-			System.out.println("El crédito no puede ser menor al saldo actual");
-			return; 
-		}
-		this.credito = credito; }
+    // Getter
+    public double getCredito() {
+        return credito;
+    }
 
-	// Sobreescribo el metodo de sacar dinero de la clase Cuenta que es abstracta y se debe implementara en todas sus hijas con su propioa función para validar el crédito
-	@Override
-	public void sacarDinero(double cantidad) {
-		if (cantidad <= 0) {
-			System.out.println("La cantidad no puede ser negativa");
-			return;
-		}
+    // Setter
+    public void setCredito(double credito) {
+        // Validación que no supere 300€
+        if (credito > 300) {
+            System.out.println("El crédito no debe superar los 300€");
+            return; // Se devuelve para que vuelva a introducir el valor, tras imprimir el mensaje de error
+        }
+        // Validación para que el saldo no sea inválido
+        if (this.saldo < -credito) { // Pongo el credito en negativo porque nos dice que podemos tener numeros rojos igual a la cantidad de crédito pero este mismo no puede ser mayor al saldo actual
+            System.out.println("El crédito no puede ser menor al saldo actual");
+            return;
+        }
+        this.credito = credito;
+    }
 
-		if (this.saldo - cantidad < -credito) { // Puedes tener la misma cantidad de credito como numero rojo en la cuenta  pero no más de ello
-			System.out.println("No se puede retirar esta cantidad, el saldo mínimo debe ser: " + (-credito) + "€");
-			return;
-		}
-		this.saldo -= cantidad;
-		System.out.println("Se han retirado " + cantidad + "€" + " correctamente");
-		System.out.println("Saldo actual: " + this.saldo + "€");
-	}
+    // Sobreescribo el metodo de sacar dinero de la clase Cuenta que es abstracta y se debe implementara en todas sus hijas con su propioa función para validar el crédito
+    @Override
+    public void sacarDinero(double cantidad) {
+        if (cantidad <= 0) {
+            System.out.println("La cantidad no puede ser negativa");
+            return;
+        }
 
-	// Método para mostrar información de la cuenta
-	public void mostrarInfo() {
-		System.out.println("Titular: " + this.titular + " ---" + " Saldo: " + this.saldo + "€" + " ---" + " Crédito: " + this.credito + "€");
-	}
+        if (this.saldo - cantidad < -credito) { // Puedes tener la misma cantidad de credito como numero rojo en la cuenta  pero no más de ello
+            System.out.println("No se puede retirar esta cantidad, el saldo mínimo debe ser: " + (-credito) + "€");
+            return;
+        }
+        this.saldo -= cantidad;
+        System.out.println("Se han retirado " + cantidad + "€" + " correctamente");
+        System.out.println("Saldo actual: " + this.saldo + "€");
+    }
+
+    // Método para mostrar información de la cuenta
+    public void mostrarInfo() {
+        System.out.println("Titular: " + this.titular + " ---" + " Saldo: " + this.saldo + "€" + " ---" + " Crédito: " + this.credito + "€");
+    }
 }
--- Java/Trimestre3/Relacion10v2/Ejercicio1/Main.java
+++ Java/Trimestre3/Relacion10v2/Ejercicio1/Main.java
@@ -14,16 +14,18 @@ con estas opciones.
 3. Mostrar saldo y crédito
 4. Salir
 Realizar un programa Principal que cree una cuenta ahorro joven y pruebe sus métodos.
-*/
+ */
 package Relacion10v2.Ejercicio1;
 
 import java.util.Scanner;
 
 public class Main {
-	private static final Scanner teclado = new Scanner(System.in);
-	public static void main (String[] args) {
-		CuentaCredito ahorrojoven = new CuentaCredito("Javier Jiménez", 400, 120); // Titular, saldo, credito
-		MenuCuentaCredito menuBBVA = new MenuCuentaCredito(ahorrojoven); // Creo el menu para la cuenta de credito creada y le paso la cuenta como parametro para que el menu pueda usar los metodos de la clase CuentaCredito
-		menuBBVA.mostrarMenu(); // Llamo al metodo mostrarMenu del objeto menuBBVA para mostrar el menu al usuario
-	}
+
+    private static final Scanner teclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        CuentaCredito ahorrojoven = new CuentaCredito("Javier Jiménez", 400, 120); // Titular, saldo, credito
+        MenuCuentaCredito menuBBVA = new MenuCuentaCredito(ahorrojoven); // Creo el menu para la cuenta de credito creada y le paso la cuenta como parametro para que el menu pueda usar los metodos de la clase CuentaCredito
+        menuBBVA.mostrarMenu(); // Llamo al metodo mostrarMenu del objeto menuBBVA para mostrar el menu al usuario
+    }
 }
